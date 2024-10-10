78abde4b50f6cd1a48ca8dbe4b3c52ea16beb2fe
Aamir041 <aamirsaudagar34@gmail.com>
2024-10-10 17:25:01 +0530

    layer output

--- src/main/java/ai/nn/StartNN.java
+++ src/main/java/ai/nn/StartNN.java
@@ -1,20 +1,30 @@
 package ai.nn;
 
+import java.util.Arrays;
+
 public class StartNN {
     public static void main(String[] args) {
-        Double[] inputs = {1.2, 5.1, 2.1};
-        Double[] weights = {3.1, 2.1, 8.7};
-        Double bias = 3d;
 
-        Double output = 0d;
+        Double[] inputs = {1d, 2d, 3d, 2.5d};
 
-        for(int i=0; i<inputs.length; i++){
-            output += (inputs[i]*weights[i]);
+        Double[][] weights = {
+                {0.2d, 0.8d, -0.5d, 1.0},
+                {0.5d, -0.91d, 0.26d, -0.5d},
+                {-0.26d, -0.27d, 0.17d, 0.87d}
+        };
+        Double[] bias = {2d,3d,0.5d};
+
+        Double[] output = new Double[weights.length];
+
+        for(int i=0; i<weights.length; i++){
+            output[i] = 0d;
+            for (int j = 0; j<weights[i].length; j++){
+               output[i] += (inputs[j] * weights[i][j]);
+           }
+           output[i] += bias[i];
         }
 
-        output += bias;
-
-        System.out.println(output);
+        System.out.println(Arrays.toString(output));
 
 
     }
